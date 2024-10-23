313146db45dc1cdb2f01980d7a9316a8e10c58ed
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-10-22 22:58:04 -0300

    update
    -continuating js (have to fix alot bugs)
    -finished html, css

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,30 +1,95 @@
-const additionBtn = document.querySelector('#addition-btn')
-const subtractionBtn = document.querySelector('#subtraction-btn')
-const multiplicationBtn = document.querySelector('#multiplication-btn')
-const divisionBtn = document.querySelector('#division-btn')
 
+const buttons = document.querySelectorAll('button');
+let calculateBtn = document.querySelector('#calculate-btn')
+let painelIndex = document.querySelector('#solution');
+let painelCalc = document.querySelector('#calculating');
 
+let n1 = ''
+let op = undefined
+let n2 = ''
 
-let n1;
-let n2;
+painelCalc.textContent = undefined
+painelIndex.textContent = undefined
 
-function calculate(n1,n2){
-    additionBtn.addEventListener('click', () => {op = additionBtn.textContent})
-    subtractionBtn.addEventListener('click', () => {op = subtractionBtn.textContent})
-    multiplicationBtn.addEventListener('click', () => {op = multiplicationBtn.textContent})
-    divisionBtn.addEventListener('click', () => {op = divisionBtn.textContent})
-
-    let res;
-    let op;
+buttons.forEach(button => {
+    let buttonValue = undefined
+    let res
     
-    switch(n1, op, n2){
+    button.addEventListener('click', (event) => {
+        buttonValue = event.target.textContent;
+        
+
+        if(!isButtonValid(buttonValue)){
+            return
+        }
+        
+        if(buttonValue == '/' || buttonValue == '*' || buttonValue == '+' || buttonValue == '-'){
+            op = buttonValue
+            res = op
+        }else if(op == undefined){
+            n1 += buttonValue
+            res = n1
+        }else if(op != undefined){
+            n2 += buttonValue
+            res = n2
+        }
+        
+        painelValue(buttonValue,res)
+        
+        console.log(`n1: ${n1} operator: ${op} n2: ${n2}`)
+    })
+})
+
+function calculate(n1,op,n2){
+    let number1 = Number(n1)
+    let number2 = Number(n2)
+    let operator = String(op)
+    let calc;
+
+    switch(operator){
         case '+':
-            res = n1 + n2
+            calc = number1 + number2
             break
         case '-':
-            res = n1 - n2
+            calc = number1 - number2
             break
+        case '*':
+            calc = number1 * number2
+            break
+        case '/':
+            calc = number1 / number2
+            break
+        default:
+        alert('[ERRO] Tente novamente mais tarde')
+        return
     }
-    alert(res)
+    painelIndex.innerHTML = '' 
+    painelValue(calc)
+
+}   
+
+
+function isButtonValid(buttonValue){
+    if(buttonValue == 'C' || buttonValue == 'CE' || buttonValue == '='){
+        return false
+    }else if(buttonValue == '/' || buttonValue == '*' || buttonValue == '+' || buttonValue == '-'){
+        return true
+    }else{
+        return true
+    }
 }
+
+function painelValue(buttonValue,res){
+    let numbersArr = [res]
+    if(!isButtonValid(buttonValue)){
+        painelIndex.textContent += ''
+    }else{
+        painelIndex.innerHTML += buttonValue
+        painelCalc.innerHTML += numbersArr
+    }
+}
+
+calculateBtn.addEventListener('click', () => {
+    calculate(n1,op,n2)
+})
 
