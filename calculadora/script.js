f5d2518bb3205e3a861c47dee6fc049cf484e5d9
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-10-24 17:42:36 -0300

    organizei um pouco o código

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,47 +2,52 @@ let previousOperationText = document.querySelector('#calculating')
 let corruentOperationText = document.querySelector('#solution')
 const buttons = document.querySelectorAll('button')
 const calculateBtn = document.querySelector('#calculate-btn')
+const clearBtn = document.querySelector('#clear-btn')
+const cancelEntryBtn = document.querySelector('#cancel-entry-btn')
 
 corruentOperationText.textContent = ''
 previousOperationText.textContent = ''
-
 
 let n1 = ''
 let operator = undefined
 let n2 = ''
 
+
+// eventos
+clearBtn.addEventListener('click', update)
+cancelEntryBtn.addEventListener('click', cancelEntry)
+calculateBtn.addEventListener('click', handleCalculate)
+
 buttons.forEach((btn) => {
     btn.addEventListener('click', (event) =>{
         let clickedButton = event.target.textContent;
-
         if(blockedButtons(clickedButton)){
             return
         }
         if(isOperator(clickedButton)){
-            operator = clickedButton
-            display(operator)
+            if(n1 == ''){
+                n1 = clickedButton
+                display(n1)
+            }else{
+                operator = clickedButton
+                display(operator)
+            }
             return
         }
         if(operator == undefined){
             n1 += clickedButton
         }
         if(operator != undefined){
+        
             n2 += clickedButton
-        }        
+        }
+        if(operator == 0){
 
+        }
         display(clickedButton)
-
-        console.log(`n1: ${n1}, op: ${operator}, n2: ${n2}`)
+        
+        console.log(`N1: ${n1}, OP: ${operator}, N2: ${n2}`)
     })
-})
-
-calculateBtn.addEventListener('click', () => {
-    let res = calculate(n1,operator,n2)
-    previousOperationText.textContent = `${n1} ${operator} ${n2} = ${res}`;
-    display(res)
-    n1 = res
-    operator = undefined
-    n2 = ''
 })
 
 function calculate(n1,op,n2){
@@ -68,8 +73,7 @@ function calculate(n1,op,n2){
         return
     }
     return res
-}   
-
+} 
 function isOperator(clickedButton){
     return clickedButton == '/' || clickedButton == '*' ||clickedButton == '-' ||clickedButton == '+' 
 }
@@ -83,3 +87,25 @@ function display(clickedButton) {
         corruentOperationText.textContent = res
     })
 }
+
+function update(){
+    n1 = ''
+    operator = undefined
+    n2 = ''
+    res = undefined
+    corruentOperationText.textContent = ''
+    previousOperationText.textContent = ''
+}
+
+function handleCalculate(){
+    if(n1 == '' || operator == undefined){
+        return
+    }
+    let res = calculate(n1,operator,n2)
+    previousOperationText.textContent = `${n1} ${operator} ${n2} = ${res}`;
+    display(res)
+    n1 = res
+    operator = undefined
+    n2 = ''
+}
+
