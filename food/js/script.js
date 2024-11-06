f5f116aab0e67da9ed283d0684db7d168600ce40
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-11-06 12:04:46 -0300

    update
    finished nav mobile
    finished navigation tabs

--- food/js/script.js
+++ food/js/script.js
@@ -1,14 +1,46 @@
 import reveal from './scroll.js'
 
 const burguer = document.querySelector('.fa-bars')
+const close = document.querySelector('.burguer-close')
+const navTag = document.querySelector('nav')
+const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll('header ul li > a');
+
+burguer.addEventListener('click', openNavigationMenu)
+close.addEventListener('click', closeNavigationMenu)
+
 
 reveal()
 
 
-burguer.addEventListener('click', openNavigationMenu)
+window.addEventListener('scroll', scrollNavitation)
+
+
 
 function openNavigationMenu(){
-    console.log('cliquei')
+    navTag.classList.add('mobile-open')
+
+    close.style.display = 'block'
+    burguer.style.display = 'none'
 }
 
-console.log(burguer)
+function closeNavigationMenu(){
+    navTag.classList.remove('mobile-open')
+    
+    close.style.display = 'none'
+    burguer.style.display = 'block'
+}
+
+function scrollNavitation(){
+    let scrollPosition = window.scrollY + 80;
+    sections.forEach(section => {
+        if (scrollPosition >= section.offsetTop) {
+            links.forEach(link =>{
+                link.classList.remove('selected')
+                if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('selected');
+                }
+            })
+        }
+    });
+}
