c60029c40cbf4649170046b5a6860edc5a4bb96d
Leleo1337 <168025120+Leleo1337@users.noreply.github.com>
2024-11-19 12:44:03 -0300

    update
    open modal working now
    also organized css for better readability

--- menu-lateral/js/main.js
+++ menu-lateral/js/main.js
@@ -1,21 +1,41 @@
 const sideBarButton = document.querySelector('#open-nav')
 const sideBar = document.querySelector('#side-bar')
 const button = document.querySelector('#open-nav > button')
-const listItens = document.querySelectorAll('ul > li')
+const listItens = document.querySelectorAll('#side-item')
 const logOutButton = document.querySelector('#logout > a')
 const logOutModal = document.querySelector('#modal-logout')
 const confirmationButtons = document.querySelectorAll('#confirmation > i')
+
 
 
 logOutButton.addEventListener('click', openModal)
 
 
 function openModal() {
-    if(sideBar.classList.contains('off') && logOutModal.classList.contains('on')){
-        logOutModal.classList.remove('on')
+    if(sideBar.classList.contains('off')){
+        sideBar.classList.replace('off', 'on')
+        logOutModal.classList.replace('off', 'on')
+    }else{
+        logOutModal.classList.replace('off', 'on')
     }
+    
+    sideBarButton.addEventListener('click', () => {
+        if(sideBar.classList.contains('off') && logOutModal.classList.contains('on')){
+            logOutModal.classList.replace('on', 'off')
+        }
+    })
 }
 
+confirmationButtons.forEach(btn => 
+    btn.addEventListener('click', (event) => {
+        let target = event.target.id
+        if(target == 'yes'){
+            window.location.reload()
+        }else{
+            logOutModal.classList.replace('on', 'off')
+        }
+    })
+)
 
 sideBarButton.addEventListener('click', () => {
     sideBar.classList.toggle("off")
@@ -25,9 +45,7 @@ sideBarButton.addEventListener('click', () => {
     }else{
         button.textContent = ">"
     }
-
 })
-
 
 listItens.forEach(item =>{
     item.addEventListener('click', () => {
@@ -35,7 +53,3 @@ listItens.forEach(item =>{
         item.classList.add('active')
     })
 })
-
-
-
-
